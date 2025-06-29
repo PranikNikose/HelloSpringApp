b3abc1f0f4094e297d58ebbc2008241050c15ec5
praniknikose <praniknikose@gmail.com>
2025-06-29 17:39:30 +0530

    Initial First & Last commit

---
+++ src/test/java/com/pranik/sb/HelloSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranik.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
